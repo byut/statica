376dbc473067dc479708ba677cc436a99b371aa2
Volodymyr But <vbut.dev+git@gmail.com>
2024-11-21 19:58:10 +0100

    feat: add binomial.cdf wrapper

--- src/statica/binomial.py
+++ src/statica/binomial.py
@@ -50,4 +50,52 @@ def pmf(n, p):
     click.echo(output)
 
 
+@click.command()
+@_option_n(name="n")
+@_option_p(name="p")
+@click.option("operator", "--lt", flag_value="lt")
+@click.option("operator", "--gt", flag_value="gt")
+@click.option("operator", "--leq", flag_value="leq", default=True)
+@click.option("operator", "--geq", flag_value="geq")
+def cdf(n, p, operator):
+    def leq(k, n, p):
+        return binom.cdf(k, n, p)
+
+    def lt(k, n, p):
+        return leq(k - 1, n, p)
+
+    def gt(k, n, p):
+        return 1 - leq(k, n, p)
+
+    def geq(k, n, p):
+        return 1 - lt(k, n, p)
+
+    probabilities = [[None, None]] * (n + 1)
+    function = None
+    signature = None
+
+    match operator:
+        case "lt":
+            function = lt
+            signature = "P(X < k)"
+        case "gt":
+            function = gt
+            signature = "P(X > k)"
+        case "leq":
+            function = leq
+            signature = "P(X <= k)"
+        case "geq":
+            function = geq
+            signature = "P(X >= k)"
+
+    for k in range(0, n + 1):
+        probabilities[k] = k, function(k, n, p).item()
+
+    output = tabulate(
+        probabilities, headers=["k", signature], colalign=("right", "left")
+    )
+    click.echo(output)
+
+
 binomial.add_command(pmf)
+binomial.add_command(cdf)
